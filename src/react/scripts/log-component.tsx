a6090360951bb92f986a0c06aae902e55bec08a6
webyom <webyom@gmail.com>
2020-06-03 21:42:53 +0800

    use reselect

--- src/react/scripts/log-component.tsx
+++ src/react/scripts/log-component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../../basic/scripts/store';
+import { createSelector } from '@reduxjs/toolkit';
 
 type ContentProps = ReturnType<typeof mapState>;
 
@@ -8,16 +9,25 @@ export const Log = (props: ContentProps): React.ReactElement => {
   return (
     <div>
       {props.logs.map((log, i) => (
-        <div key={log.time + '_' + i}>
-          {new Date(log.time).toLocaleString()} {log.type}
-        </div>
+        <div key={i}>{log}</div>
       ))}
     </div>
   );
 };
 
+const getLogs = createSelector(
+  (state: RootState) => state.log,
+  log => {
+    return {
+      logs: log.logs.map(log => {
+        return `${new Date(log.time).toLocaleString()} ${log.type}`;
+      }),
+    };
+  },
+);
+
 const mapState = (state: RootState) => {
-  return state.log;
+  return getLogs(state);
 };
 
 export const LogComponent = connect(mapState)(Log);
