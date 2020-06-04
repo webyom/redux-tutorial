3167951163c4beb8ed526564b7c8cb11a204289a
webyom <webyom@gmail.com>
2020-06-04 22:33:38 +0800

    use redux hooks

--- src/react/scripts/count-component.tsx
+++ src/react/scripts/count-component.tsx
@@ -1,50 +1,40 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { AppDispatch, RootState } from '../../basic/scripts/store';
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../basic/scripts/store';
 import { increase, decrease, fetchCount } from '../../basic/scripts/count-slice';
 import { log } from '../../basic/scripts/log-slice';
 
-type ContentProps = ReturnType<typeof mapDispatch> & ReturnType<typeof mapState>;
+export const CountComponent = (): React.ReactElement => {
+  const dispatch = useDispatch();
+  const count = useSelector((state: RootState) => state.count);
+  const doFetchCount = useCallback(() => dispatch(fetchCount()), []);
+  const doIncrease = useCallback(() => {
+    dispatch(increase(1));
+    dispatch(
+      log({
+        type: 'increase',
+        time: Date.now(),
+      }),
+    );
+  }, []);
+  const doDecrease = useCallback(() => {
+    dispatch(decrease(1));
+    dispatch(
+      log({
+        type: 'decrease',
+        time: Date.now(),
+      }),
+    );
+  }, []);
 
-export const Count = (props: ContentProps): React.ReactElement => {
   useEffect(() => {
-    props.fetchCount();
+    doFetchCount();
   }, []);
 
   return (
     <React.Fragment>
-      <div>{props.error ? props.error.message : props.loading ? 'Loading...' : props.count}</div>
-      <button onClick={props.increase}>Increase</button> <button onClick={props.decrease}>Decrease</button>
+      <div>{count.error ? count.error.message : count.loading ? 'Loading...' : count.count}</div>
+      <button onClick={doIncrease}>Increase</button> <button onClick={doDecrease}>Decrease</button>
     </React.Fragment>
   );
 };
-
-const mapState = (state: RootState) => {
-  return state.count;
-};
-
-const mapDispatch = (dispatch: AppDispatch) => {
-  return {
-    increase: () => {
-      dispatch(increase(1));
-      dispatch(
-        log({
-          type: 'increase',
-          time: Date.now(),
-        }),
-      );
-    },
-    decrease: () => {
-      dispatch(decrease(1));
-      dispatch(
-        log({
-          type: 'decrease',
-          time: Date.now(),
-        }),
-      );
-    },
-    fetchCount: () => dispatch(fetchCount()),
-  };
-};
-
-export const CountComponent = connect(mapState, mapDispatch)(Count);
